f1049e294b80a5a29924aa19d3a163be4495e031
Mauro Bogliaccino <mauro.bogliaccino@gmail.com>
2026-01-29 13:00:38 +0100

    agg

--- codice/Lez20260129/studenti.py
+++ codice/Lez20260129/studenti.py
@@ -1,15 +1,36 @@
 studenti = []
 
-f = open('./studenti.txt')
+f = open('studenti.txt')
 
 for riga in f:
     # print(riga)
     # per ogni riga del file aggiungo la riga alla lista studenti
+    riga = riga.replace("\n", "")
+    riga = riga.replace("\t", ",")
     studenti.append(riga)
 
 f.close()
 
-for s in studenti:
-    print(s)
+f = open('studenti.sql', 'w')
 
+query_tabella = """
+DROP TABLE IF EXISTS studenti;\n\n
+
+CREATE TABLE studenti(\n
+    id int primary key auto_increment,\n
+    nome varchar(30) not null,\n
+    cognome varchar(50) not null\n
+);\n\n
+
+"""
+f.write(query_tabella)
+
+for s in studenti:
+    s = str(s)
+    pezzi = s.split(",")
+    nome = pezzi[0]
+    cognome = pezzi[1]
+    f.write(f"insert into studenti (nome, cognome) value ('{nome}', '{cognome}');\n")
+    # print(f"Il nome dello studente è {nome} e il cognome è {cognome}")
+f.close()
 
