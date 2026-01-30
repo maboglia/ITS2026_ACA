30fcbb88fc345e755cdb2739a4e21e795f6ef5bb
Mauro Bogliaccino <mauro.bogliaccino@gmail.com>
2026-01-30 11:20:39 +0100

    lez 20260130

---
+++ database/daily_warm_up.sql
@@ -0,0 +1,28 @@
+# creare utente
+
+
+# creare database
+
+
+# assegnare permessi a utente su database
+
+
+# connessione da utente a database
+
+
+# creazione di tabella
+
+
+# inserimento / lettura / modifica  / eliminazione record
+
+
+# eliminazione tabella
+
+
+# revocare privilegi
+
+
+# eliminare database
+
+
+# eliminare utente
