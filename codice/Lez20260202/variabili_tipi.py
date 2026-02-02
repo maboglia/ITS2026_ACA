d2324b79586983f080d0f7488758ee3344765499
Mauro Bogliaccino <mauro.bogliaccino@gmail.com>
2026-02-02 15:14:34 +0100

    laboratorio variabili e tipi

---
+++ codice/Lez20260202/variabili_tipi.py
@@ -0,0 +1,57 @@
+
+# nomi = ['mauro', 'paolo', 'paola', 25]
+
+# for nome in nomi:
+#     print(nome)
+
+
+
+# nome: str = "Mauro"
+
+# nome = 'paola'
+
+# nome = 7
+
+
+# print(type("Mauro"))
+
+# a = 5
+# b = 2
+
+# print(a + b) # 7   
+# print(a - b) # 3   
+# print(a * b) # 10   
+# print(a / b) # 2.5    # ho 2 panini, li divido tra 2 bambini: ogni bambino si becca 2 panini e mezzo
+# print(a // b) # 2    # ho 5 caramelle, le divido tra 2 bambini: ogni bambino ha 2 caramelle e ne resta una
+# print(a % b) # 1    # resto della divisione tra 5 caramelle e 2 bambini
+
+
+# print(a == b) # False
+# print(a != b) # True
+# print(a > b) # True
+# print(a <= b) # False
+      
+# username = input("Come ti chiami? ")
+# print(f"Benvenuto utente {username}")
+# stelle = input("Ti piace questo programma, quante stelline daresti? ")
+
+# stelle = "1" + stelle
+
+# print(f"Grazie per avermi dato {stelle} stelle!")
+
+#        0123456789
+testo = "  Python è Fantastico  "
+
+# # print(testo.upper()) #   PYTHON È FANTASTICO  
+# # print(testo.lower()) #   python è fantastico  
+# # print(testo.strip()) # Python è Fantastico
+# # print(testo.replace("Fantastico", "potente")) #   Python è potente
+      
+# print(testo)
+# testo = testo.strip()      
+# print(testo)
+
+print(testo[5:10])
+print(testo[5:])
+print(testo[:12:2])
+print(testo[::-1])
