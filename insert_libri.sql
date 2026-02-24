d65794957b71eb287b03844447230c7339409b0b
Mauro Bogliaccino <mauro.bogliaccino@gmail.com>
2026-02-24 09:07:48 +0100

    movimenti di files

--- insert_libri.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO libri SET titolo = 'L\'uomo ragno', pagine = 120, prezzo = 15, editore_id = 2;
-INSERT INTO libri SET titolo = 'La donna ragno', pagine = 150, prezzo = 25, editore_id = 2;
-INSERT INTO libri SET titolo = 'Il bambino ragno', pagine = 220, prezzo = 35, editore_id = 2;
-INSERT INTO libri SET titolo = 'L\'insetto verde', pagine = 80, prezzo = 10, editore_id = 1;
-INSERT INTO libri SET titolo = 'L\'opossum grigio', pagine = 320, prezzo = 45, editore_id = 1;
