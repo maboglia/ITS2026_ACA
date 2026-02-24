f9f42b9f5852ded033669aa0eb2da6661c67b4a5
maboglia <mauro.bogliaccino@gmail.com>
2026-02-24 16:10:05 +0100

    lez 24.2.26

--- codice/Lez20260224/geografia.py
+++ codice/Lez20260224/geografia.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 """ Regioni italiane """
 
 regioni = {
@@ -17,22 +16,4 @@ valori = regioni.values()
 
 for regione, comuni in regioni.items():
     print(f"La regione {regione} ha i seguenti comuni capoluogo di provincia {comuni}")
-=======
-"""Regioni Italiane """
 
-regioni ={
-    "Piemonte" : ["Torino", "Cuneo", "Asti", "Alessandria"],
-    "Liguria" : ["Genova", "Savona", "Imperia", "La Spezia"]
-}
-
-# print(help(regioni))
-chiavi = regioni.keys()
-valori = regioni.values()
-
-# for chiavi in chiavi:
-#     print(chiavi)
-#     print(regioni.get(chiavi))
-
-for regione, comuni in regioni.items():
-    print(f"La regione {regione} ha i segueti comuni capoluogo di provincia {comuni}")
->>>>>>> 6946f19a7cc5421ce8fad76876fed9c3087c69d1
